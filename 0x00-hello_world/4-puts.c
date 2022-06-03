68d3487c85b5f39dfda3c70ff9dc3a17fbdae1f1
TeamirA <teamir124@gmail.com>
2022-06-02 18:13:05 -0700

    did some error editing on some files, no biggie

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,11 @@
 #include <stdio.h>
 /**
- * main -program entry point
+ * main - program entry point
  * 
- * return: 0 (exit main function)
+ * Return: 0 always (exit main function)
  */
 int main(void)
 {
-	puts("\"Programing is like building a multilingual puzzle");
+	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
 }
