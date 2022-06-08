2e0e9b5cf50323174f45c843eced34f870acf745
TeamirA <teamir124@gmail.com>
2022-06-07 17:01:47 -0700

    fixed some error, betty doc style

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,16 +1,16 @@
 #include "main.h"
 
 /**
- * main - Entry point 
- *
+ * print_alphabet - a function that prints the alphabet in lowercase
+ * 
  * Return: Always 0 (Success)
  */
- void print_alphabet(void)
- {
- 	char c;
+void print_alphabet(void)
+{
+	char c;
 	for (c = 'a'; c <= 'z'; c++)
 	{
 		_putchar(c);
 	}
 	_putchar('\n');
- }
+}
