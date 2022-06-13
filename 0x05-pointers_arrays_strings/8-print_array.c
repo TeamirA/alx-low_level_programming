16a8e4d9f8985b713c1569be984d3e0930f9fc67
TeamirA <teamir124@gmail.com>
2022-06-13 11:35:36 -0700

    edited file 8-print_array for some error occured during compilation

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -1,4 +1,5 @@
 #include "main.h"
+#include <stdio.h>
 /**
  * print_array - print n of int array
  * @a: pointer
@@ -8,10 +9,10 @@ void print_array(int *a, int *b)
 {
 	short c = 0;
 
-	while (n-- > 0)
+	while (b-- > 0)
 	{
 		printf("%d", a[c++]);
-		if (n != 0)
+		if (b != 0)
 			printf(", ");
 	}
 	printf("\n");
