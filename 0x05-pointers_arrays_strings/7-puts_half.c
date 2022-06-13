133f160d2b889ee296c82e8e4bd6c0ad6d05efaf
TeamirA <teamir124@gmail.com>
2022-06-13 11:26:54 -0700

    edited some indentation error of file 7-puts_half

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -7,16 +7,19 @@ void puts_half(char *str)
 {
 	int len, n, i;
 	len = 0;
+
 while (str[len] != '\0')
 {
 	len++;
 }
 if (len % 2 == 0)
-{ for (i = len / 2; str[i] != '\0'; i++)
+{
+	for (i = len / 2; str[i] != '\0'; i++)
 	{
 		_putchar(str[i]);
 	}
-} else if (len % 2)
+}
+else if (len % 2)
 	{
 		for (n = (len - 1) / 2; n < len - 1; n++)
 		{
